b66e956b0fe08488eb614cfdf0b3b500165f6320
jesus <therexby2@gmail.com>
2024-08-17 21:29:08 -0600

    Initial Commit

---
+++ Que personaje soy/Que_personaje_soyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Que_personaje_soyApp.swift
+//  Que personaje soy
+//
+//  Created by Jesus Rafael Valdes on 17/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Que_personaje_soyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Que personaje soyUITests/Que_personaje_soyUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Que_personaje_soyUITestsLaunchTests.swift
+//  Que personaje soyUITests
+//
+//  Created by Jesus Rafael Valdes on 17/08/24.
+//
+
+import XCTest
+
+class Que_personaje_soyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
