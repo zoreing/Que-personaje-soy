5e3a6a6addab9d735b8106e37c843b5b66f3d464
zoreing <therexby2@gmail.com>
2024-08-19 15:54:07 -0600

    poner la pregunta dos en el contentvIEW Principal

--- Que personaje soy/ContentView.swift
+++ Que personaje soy/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
         VStack {
             NavigationView {
                 List{
-                    NavigationLink(destination: PreguntaDosView()){
+                    NavigationLink(destination: PreguntaDosDosView()){
                     Button("Hombre") {
                         contadorPreguntas.setCounts(count: 0,num :1)
                     }}
@@ -36,6 +36,39 @@ struct ContentView: View {
     }
     
 }
+
+
+
+
+struct PreguntaDosDosView: View {
+    @StateObject var contadorPreguntas = contadorViewModel()
+    var body: some View {
+        
+        
+        VStack {
+            NavigationView {
+                List{
+                Text("La eficiencia y la lógica para encontrar la mejor solución.")     //A
+                Text("El coraje y la acción inmediata para resolver el problema.")      //B
+                Text("La moralidad y el impacto positivo en los demás")                 //C
+                Text("La estrategia a largo plazo y el éxito del grupo.s")              //D
+                    Text(String(contadorPreguntas.contadorPreguntas[0]))
+            }
+    
+    
+                }
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarHidden(true)
+            .padding()
+        }
+    }
+    
+}
+
+
+
+
+
 final class contadorViewModel : ObservableObject{
     @Published var contadorPreguntas = [Int](repeating: 0, count: 5)
     func setCounts(count: Int, num : Int){
