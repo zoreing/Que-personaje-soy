09f82c293f43f2ef35e5dec78a3918b7e1357c8b
zoreing <therexby2@gmail.com>
2024-08-18 23:16:13 -0600

    agregando el contenido de las views en un comentario

--- Que personaje soy/ContentView.swift
+++ Que personaje soy/ContentView.swift
@@ -33,3 +33,68 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+/*
+        VStack {
+            NavigationView {
+                List{
+                Text("La eficiencia y la lógica para encontrar la mejor solución.")
+                Text("El coraje y la acción inmediata para resolver el problema.")
+                Text("La moralidad y el impacto positivo en los demás")
+                Text("La estrategia a largo plazo y el éxito del grupo.s")
+                
+            }
+            .navigationTitle("¿Qué valoras más en una situación difícil?")
+            .navigationBarTitleDisplayMode(.inline)
+                }
+            .padding()
+        }
+
+
+        VStack {
+            NavigationView {
+                List{
+                Text("Reflexionando y meditando antes de actuar.")          \\a
+                Text("Enfrentándolo de manera directa y sin rodeos.")       \\b
+                Text("Buscando una solución pacífica y diplomática.")       \\c
+                Text("Liderando y guiando a los demás hacia la solución.")  \\d
+                
+            }
+            .navigationTitle("¿Cómo prefieres abordar un conflicto?")
+            .navigationBarTitleDisplayMode(.inline)
+                }
+            .padding()
+        }
+
+
+        VStack {
+            NavigationView {
+                List{
+                Text("El de sabio consejero, ofreciendo guía desde la experiencia.")        \\A
+                Text("El de héroe valiente, tomando riesgos para el éxito del equipo.")     \\B
+                Text("El de protector, asegurándote de que todos estén bien y seguros.")    \\C
+                Text("El de líder, tomando decisiones estratégicas para el bien común.")    \\D
+                
+            }
+            .navigationTitle("¿Qué rol prefieres asumir en un equipo?")
+            .navigationBarTitleDisplayMode(.inline)
+                }
+            .padding()
+        }
+
+        VStack {
+            NavigationView {
+                List{
+                Text("Sabio y espiritual, con una profunda conexión con el universo.")          \\A
+                Text("Aventurero y audaz, con un enfoque pragmático en la vida.")               \\B
+                Text("Compasivo y justo, siempre buscando lo mejor para los demás.")            \\C
+                Text("Fuerte y decidido, con un enfoque en el liderazgo y la justicia.")        \\D
+                
+            }
+            .navigationTitle("¿Cómo te describirían mejor?")
+            .navigationBarTitleDisplayMode(.inline)
+                }
+            .padding()
+
+*/
